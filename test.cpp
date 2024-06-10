e5f35f775434a1ef630b4a384b41dbdcb899065c
Misha Spiridonov <jacknap1er@mail.ru>
2024-06-10 20:52:04 +0300

    Первый комиит

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+<<<<<<< HEAD
+ cout << "Hello World:):)" << endl;
+ cout << "третий коммит" << endl; 
+ cout << "гитхаб потерял коммиты";
+=======
+ cout << "Hello World" << endl;
+ cout << "Git version 2" << endl;
+>>>>>>> e76a8f8183fa8703906d0c423c0b0b8114685b97
+}
